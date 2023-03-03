4a2a5f86ab1c5012ee4cb0eb1506fe4e6c5bc81c
Tonny Oseko <tonnyoseko0@gmail.com>
2023-03-03 16:11:04 +0300

    create the User model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,12 @@ const Sequelize = require("sequelize");
 const db = require("../util/database");
 
 const User = db.define("user", {
-  id: {},
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
   name: Sequelize.STRING,
   email: Sequelize.STRING,
 });
